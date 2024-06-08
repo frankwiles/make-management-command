81f2530e493f2acf4e1b4e994ba7f640157f1bb1
Frank Wiles <frank@revsys.com>
2024-06-08 15:54:45 -0500

    Fix tests

--- tests/test_make_management_command.py
+++ tests/test_make_management_command.py
@@ -1,10 +1,29 @@
 from click.testing import CliRunner
+
 from make_management_command.cli import cli
 
 
-def test_version():
+def test_no_command():
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = runner.invoke(cli, ["--version"])
+        result = runner.invoke(
+            cli,
+        )
         assert result.exit_code == 0
-        assert result.output.startswith("cli, version ")
+        assert result.output.startswith("Checking for ")
+
+
+def test_one_new_command():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, "test1")
+        assert result.exit_code == 0
+        assert result.output.startswith("Checking for ")
+
+
+def test_two_new_commands():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, "test1 test2")
+        assert result.exit_code == 0
+        assert result.output.startswith("Checking for ")
